d320607759701b072725c5f22c88cc13e8f1e2d8
Eric Ahn <wchill1337@gmail.com>
2016-03-04 04:59:07 +0000

    Complete

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,35 @@
-// Write any custom javascript functions here
+$('ul.menu > li > a').on('click', function(e) {
+  e.preventDefault();
+  var id= $(this).attr('section');
+  var navbarHeight = 41;
+  var targetPosition = $('#' + id).offset().top;
+  targetPosition -= navbarHeight;
+  $('html, body').animate({scrollTop: targetPosition}, 500);
+});
+
+$('.carousel').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
